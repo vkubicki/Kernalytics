6786e8766dea80e55ec36e40677b8ec72a5dae43
Vincent KUBICKI <vincent.kubicki@laposte.net>
2019-09-01 18:11:21 +0200

    Kernel.R new structure compiles

--- src/main/scala/rkhsnew/Kernel.scala
+++ src/main/scala/rkhsnew/Kernel.scala
@@ -2,6 +2,7 @@ package rkhsnew
 
 import breeze.linalg.{DenseVector, sum}
 import rkhsnew.AlgebraAbstract.Traits
+import various.Error
 import various.TypeDef._
 
 import scala.util.{Failure, Success, Try}
@@ -14,23 +15,35 @@ import scala.util.{Failure, Success, Try}
  * - https://en.wikipedia.org/wiki/Positive-definite_kernel , for the notations used in the kernels
  */
 object Kernel {
+
   object R {
-      object InnerProductSpace extends Traits.InnerProductSpace[Real] {
-        def minus(x: Real, y: Real): Real = x - y
-        def ip(x: Real, y: Real): Real = x * y
+
+    object InnerProductSpace extends Traits.InnerProductSpace[Real] {
+      def minus(x: Real, y: Real): Real = x - y
+
+      def ip(x: Real, y: Real): Real = x * y
+    }
+
+    val NormedSpace: Traits.NormedSpace[Real] = Traits.NormedSpaceFromInnerProductSpace(InnerProductSpace)
+
+    val MetricSpace: Traits.MetricSpace[Real] = Traits.MetricSpaceFromInnerProductSpace(InnerProductSpace)
+
+    def getKernel(inStr: String): Try[(Real, Real) => Real] = {
+      val inArray: Array[String] = inStr.split(".")
+      val structureName: String = inArray(0)
+
+      for {
+        _ <- Error.validate(inArray, inArray.length != 2, "For real numbers, the kernel name must follow the structure sutructure.kernel, for example InnerProductSpace.linear().")
+        (kernelStr, paramStr) <- io.ReadParam.parseParam(structureName)
+        res <- {
+          structureName match {
+            case "InnerProductSpace" => InnerProductSpace(kernelStr, paramStr)
+            case _ => Failure(new Exception(s"For real numbers, the structure / kernel ${inArray(0)} has not been implemented yet."))
+          }
+        }
+      } yield {
+        res
       }
-
-      val NormedSpace: Traits.NormedSpace[Real] = Traits.NormedSpaceFromInnerProductSpace(InnerProductSpace)
-
-      val MetricSpace: Traits.MetricSpace[Real] = Traits.MetricSpaceFromInnerProductSpace(InnerProductSpace)
-
-    val kerlist = Map[String, Try[(Real, Real) => Real]]()
+    }
   }
-
-
-
-/*  InnerProduct[Data](str: String, i: AlgebraAbstract.Traits.InnerProductSpace[Data]): Try[(Data, Data) => Real] = str match {
-    case "linear" => Success((x: Data, y: Data) => i.ip(x, y))
-    case _ => Failure(new Exception(s"$str not found in InnerProduct"))
-  }*/
 }
